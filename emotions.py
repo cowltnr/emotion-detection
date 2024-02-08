75c9b1e89256080cc6b3ffa56cbb3a09501ae887
js0301 <128969531+js0301@users.noreply.github.com>
2024-02-08 20:52:55 +0900

    python-final

--- emotions.py
+++ emotions.py
@@ -11,37 +11,42 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import os
 import time
+import serial
+
+out_cntmax = ""
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # command line argument
 ap = argparse.ArgumentParser()
-ap.add_argument("--mode",help="train/display")
+ap.add_argument("--mode", help="train/display")
 a = ap.parse_args()
-mode = a.mode 
+mode = a.mode
+
 
 def plot_model_history(model_history):
     """
     Plot Accuracy and Loss curves given the model_history
     """
-    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
     # summarize history for accuracy
-    axs[0].plot(range(1,len(model_history.history['acc'])+1),model_history.history['acc'])
-    axs[0].plot(range(1,len(model_history.history['val_acc'])+1),model_history.history['val_acc'])
+    axs[0].plot(range(1, len(model_history.history['acc']) + 1), model_history.history['acc'])
+    axs[0].plot(range(1, len(model_history.history['val_acc']) + 1), model_history.history['val_acc'])
     axs[0].set_title('Model Accuracy')
     axs[0].set_ylabel('Accuracy')
     axs[0].set_xlabel('Epoch')
-    axs[0].set_xticks(np.arange(1,len(model_history.history['acc'])+1),len(model_history.history['acc'])/10)
+    axs[0].set_xticks(np.arange(1, len(model_history.history['acc']) + 1), len(model_history.history['acc']) / 10)
     axs[0].legend(['train', 'val'], loc='best')
     # summarize history for loss
-    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
-    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].plot(range(1, len(model_history.history['loss']) + 1), model_history.history['loss'])
+    axs[1].plot(range(1, len(model_history.history['val_loss']) + 1), model_history.history['val_loss'])
     axs[1].set_title('Model Loss')
     axs[1].set_ylabel('Loss')
     axs[1].set_xlabel('Epoch')
-    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+    axs[1].set_xticks(np.arange(1, len(model_history.history['loss']) + 1), len(model_history.history['loss']) / 10)
     axs[1].legend(['train', 'val'], loc='best')
     fig.savefig('plot.png')
     plt.show()
+
 
 # Define data generators
 train_dir = 'data/train'
@@ -52,27 +57,27 @@ num_val = 7178
 batch_size = 64
 num_epoch = 50
 
-train_datagen = ImageDataGenerator(rescale=1./255)
-val_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+val_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
-        train_dir,
-        target_size=(48,48),
-        batch_size=batch_size,
-        color_mode="grayscale",
-        class_mode='categorical')
+    train_dir,
+    target_size=(48, 48),
+    batch_size=batch_size,
+    color_mode="grayscale",
+    class_mode='categorical')
 
 validation_generator = val_datagen.flow_from_directory(
-        val_dir,
-        target_size=(48,48),
-        batch_size=batch_size,
-        color_mode="grayscale",
-        class_mode='categorical')
+    val_dir,
+    target_size=(48, 48),
+    batch_size=batch_size,
+    color_mode="grayscale",
+    class_mode='categorical')
 
 # Create the model
 model = Sequential()
 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -90,14 +95,14 @@ model.add(Dense(7, activation='softmax'))
 
 # If you want to train the same model or try other models, go for this
 if mode == "train":
-    model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
 
     model_info = model.fit_generator(
-            train_generator,
-            steps_per_epoch=num_train // batch_size,
-            epochs=num_epoch,
-            validation_data=validation_generator,
-            validation_steps=num_val // batch_size)
+        train_generator,
+        steps_per_epoch=num_train // batch_size,
+        epochs=num_epoch,
+        validation_data=validation_generator,
+        validation_steps=num_val // batch_size)
 
     plot_model_history(model_info)
     model.save_weights('model.h5')
@@ -116,7 +121,7 @@ elif mode == "display":
     cap = cv2.VideoCapture(0)
     start_time = time.time()
 
-    class_counts =[]
+    class_counts = []
     while True:
         # Find haar cascade to draw bounding box around face
         ret, frame = cap.read()
@@ -124,15 +129,16 @@ elif mode == "display":
             break
         facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+        faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
             roi_gray = gray[y:y + h, x:x + w]
             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
             prediction = model.predict(cropped_img)
             maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2, cv2.LINE_AA)
+            cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (10, 10, 10),
+                        2, cv2.LINE_AA)
 
             print("Emotion:", emotion_dict[maxindex])
 
@@ -146,7 +152,9 @@ elif mode == "display":
             with open(file_path, 'w') as file:
                 file.write(cnt_max)
 
-        cv2.imshow('Video', cv2.resize(frame,(800,500),interpolation=cv2.INTER_CUBIC))
+            out_cntmax = cnt_max
+
+        cv2.imshow('Video', cv2.resize(frame, (800, 500), interpolation=cv2.INTER_CUBIC))
 
         elapsed_time = time.time() - start_time
 
@@ -155,5 +163,36 @@ elif mode == "display":
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    cap.release()
-    cv2.destroyAllWindows()
+    # 아두이노에 연결된 시리얼 포트를 확인하고 포트 이름을 입력
+    arduino_port = 'COM3'  # 포트 이름에 따라 변경
+
+    # 시리얼 통신 설정
+    ser = serial.Serial(arduino_port, 9600, timeout=1)
+    time.sleep(2)  # 아두이노와 연결될 시간을 줍니다.
+
+    # # 텍스트 파일 읽기
+    # file_path = 'C:/Users/User/emotion-detection/emotions.txt'
+    # with open(file_path, 'r') as file:
+    #     data = file.read()
+
+
+    # 데이터 전송
+    def write_read():
+        ser.write(bytearray(out_cntmax,'utf-8'))
+        time.sleep(0.1)
+        data = ser.readline()
+        return data
+
+    while True:
+
+        if(True):
+            for i in range(2):
+                write_read()
+                print(out_cntmax)
+        break
+
+    # 연결 닫기
+    ser.close()
+
+cap.release()
+cv2.destroyAllWindows()
